dfd9a6bbc52d54751577ab552d1d4565d94cacde
ysadamt <adam.teo321@gmail.com>
2024-08-25 16:26:30 -0500

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head><title>HH 2024</title></Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
